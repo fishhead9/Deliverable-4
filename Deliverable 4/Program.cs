2e8d04bfaa4a54c032589e1c5f78c2895596da10
fishhead9 <122698390+fishhead9@users.noreply.github.com>
2023-02-04 16:14:52 -0500

    Add files via upload

---
+++ Deliverable 4/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 
+ Author: Nicholas Johnston
+ Date: 2/4/2023
+ Comments: Deliverable made to show the first 25 numbers of the Fibonacci sequence
+*/
+
+using System;
+class Program
+{
+    public static int Fibonacci(int nno)
+    {
+        int num1 = 0;
+        int num2 = 1;
+
+        for (int i = 0; i < nno; i++)
+        {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp + num2;
+        }
+        return num1;
+    }
+    public static void Main()
+    {
+        int n = 25;
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine();
+            Console.Write("Fibonacci number: " + Fibonacci(i) + "  ");
+        }
+        Console.WriteLine();
+    }
+}
